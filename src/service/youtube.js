c0fa89ec4a23053fa61736789ed24f91d1e67927
sunghyun <amakasi87@gmail.com>
2021-01-19 18:01:46 +0900

    비디오 클릭시 디테일추가

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -8,18 +8,18 @@ class Youtube {
 
     }
 
-    mostPopular() {
-      return fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&maxResults=25&chart=mostPopular&type=video&key=${this.key}`, this.requestOptions)
-      .then(response => response.json())
-      .then(result => result.items);
+    async mostPopular() {
+      const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&maxResults=25&chart=mostPopular&type=video&key=${this.key}`, this.requestOptions);
+        const result = await response.json();
+        return result.items;
     }
 
-    search(query) {
+    async search(query) {
         let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${this.key}&q=${query}`;
 
-        return fetch(url, this.requestOptions)
-        .then(response => response.json())
-        .then(result => result.items.map(item => ({...item, id: item.id.videoId})));
+        const response = await fetch(url, this.requestOptions);
+        const result = await response.json();
+        return result.items.map(item => ({ ...item, id: item.id.videoId }));
     }
 }
 
